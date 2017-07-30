b46ba7e0f3b8c11269d472240b7133414553bb23
Caylin James <caylinjames.dev@gmail.com>
2017-07-29 21:32:48 -0400

    Starting styles

--- functions.php
+++ functions.php
@@ -4,3 +4,5 @@
 * Add your own functions here. You can also copy some of the theme functions into this file. 
 * Wordpress will use those functions instead of the original functions then.
 */
+
+add_theme_support('avia_template_builder_custom_css');
